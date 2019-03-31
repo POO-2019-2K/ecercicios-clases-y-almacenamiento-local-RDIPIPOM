b5a2140724bfdb0d1d697bc278862ee2a3102df4
RDIPIPOM <ezamora2@ucol.mx>
2019-03-31 11:07:19 -0600

    Segunda Actualización

--- Ejercicio1/Main.js
+++ Ejercicio1/Main.js
@@ -1,10 +1,11 @@
 export default class Main {
     constructor() {
-        localStorage.removeItem('students');
-        let studentsArray = new Array();
+        this._table = document.querySelector('#tableStudents');
+        this._studentsArray = new Array();
 
         if (localStorage.getItem('students') != null) {
-            studentsArray = JSON.parse(localStorage.getItem('students'));
+            this._studentsArray = JSON.parse(localStorage.getItem('students'));
+            this._initTable();
         }
 
         document.querySelector('#btnAdd').addEventListener('click', () => {
@@ -17,7 +18,7 @@ export default class Main {
                 name: inputName.value
             }
 
-            studentsArray.forEach((student) => {
+            this._studentsArray.forEach((student) => {
                 if (student.numberAccount == objStudent.numberAccount) {
                     isCreate = true;
                 }
@@ -27,13 +28,35 @@ export default class Main {
             inputName.value = '';
 
             if (!isCreate) {
-                studentsArray.push(objStudent);
-                localStorage.setItem('students', JSON.stringify(studentsArray));
+                this._studentsArray.push(objStudent);
+                localStorage.setItem('students', JSON.stringify(this._studentsArray));
                 alert('¡Alumno registrado exitosamente!');
+                this._showInTable(objStudent);
             } else {
                 alert('El alumno ya fue registrado');
             }
+            
         });
+    }
+
+    _initTable() {
+        let row;
+        let cell;
+        this._studentsArray.forEach((student => {
+            row = this._table.insertRow(-1);
+            cell = row.insertCell(0);
+            cell.innerHTML = student.numberAccount;
+            cell = row.insertCell(1);
+            cell.innerHTML = student.name;
+        }));
+    }
+
+    _showInTable(objStudent) {
+        let row = this._table.insertRow(-1);
+        let cell = row.insertCell(0);
+        cell.innerHTML = objStudent.numberAccount;
+        cell = row.insertCell(1);
+        cell.innerHTML = objStudent.name;
     }
 }
 
